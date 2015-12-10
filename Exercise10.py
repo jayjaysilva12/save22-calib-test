49251cece159bd384585d8a835e88e4d5b8f7432
jayjaysilva <jethrojundelossantos@gmail.com>
2015-12-09 21:11:05 -0500

    Improved the sorting feature

--- Exercise10.py
+++ Exercise10.py
@@ -1,6 +1,6 @@
 import turtle
 t=turtle
-words=[('pedro',22),('juan',23),('pepe',24)]
+words={'pedro':22,'juan':23,'pepe':24}
 
 def byName(key):
     return key[0]
@@ -12,6 +12,9 @@ def turtle(val):
 	t.pensize(3)
 	t.color('red')
 	for i in range(0,val):
+		if val == 1:
+			t.color("black", "red")
+			t.begin_fill()
 		t.left(135)
 		t.forward(150)
 		t.right(90)
@@ -36,6 +39,8 @@ def turtle(val):
 		t.forward(45)
 		t.right(90)
 		t.forward(150)
+		if val == 1:
+			t.end_fill()
 
 	t.exitonclick()
 	exit()
@@ -81,30 +86,32 @@ def main():
 		if ch == 1:
 			while 1:
 				print 
-				print '[1]lambda by name'
-				print '[2]lambda by age'
-				print '[3]function by name'
-				print '[4]function by age'
+				print '[1]lambda by name in Ascending order'
+				print '[2]lambda by age in Ascending order'
+				print '[3]function by name in Descending order'
+				print '[4]function by age in Descending order'
 				print '[5]back'
 				print '[ELSE EXIT]'
 				choice = input('Enter Choice: ')
+				toPrintDict={}
 				if choice == 1:
 					print 
-					toPrint=sorted(words, key=lambda x:x[0])
+					toPrintDict=sorted(words.items(), key=lambda x:x[0], reverse = False)
 				elif choice == 2:
 					print 
-					toPrint=sorted(words, key=lambda x:x[1])
+					toPrintDict=sorted(words.items(), key=lambda x:x[1], reverse = False)
 				elif choice == 3:
 					print
-					toPrint=sorted(words, key = byName,reverse = True)
+					toPrintDict=sorted(words.items(), key = byName, reverse = True)
 				elif choice == 4:
 					print
-					toPrint=sorted(words, key = byAge,reverse = True)
+					toPrintDict=sorted(words.items(), key = byAge, reverse = True)
 				elif choice == 5:
 					main()
 				else:
 					exit()
-				print toPrint
+				for x in toPrintDict:
+					print str(x[0])+ ' ' +str(x[1])
 		elif ch == 2:
 			print 
 			print '[1]Letter X'
